ab97dbe71acb1991ce9c989c0af687bb04239497
pdnminh99 <pdn.minh1999@gmail.com>
2020-09-16 18:30:41 +0700

    add pretter default config, add angular language service dev-dependencies

---
+++ src/app/feature_modules/quiz/components/quiz-nav/quiz-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'app-quiz-nav',
+  template: ` <div>{{ quizName }} - {{ exerciseName }}</div> `,
+  styles: [``],
+})
+export class QuizNavComponent {
+  @Input()
+  quizName: string = 'Name'
+
+  @Input()
+  exerciseName: string = 'execerise name'
+}
--- src/app/feature_modules/quiz/quiz.module.ts
+++ src/app/feature_modules/quiz/quiz.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: []
+  exports: [],
+  declarations: [],
 })
-export class QuizModule {
-
-}
+export class QuizModule {}
