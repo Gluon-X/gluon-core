5e5dfee58e8aa728d30f9e85197ea713ebbd97f5
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-10-05 00:39:13 +0700

    answear list view

--- src/app/feature_modules/quiz/components/answers-type/list/answer-type-list.component.ts
+++ src/app/feature_modules/quiz/components/answers-type/list/answer-type-list.component.ts
@@ -6,8 +6,7 @@ import { BaseQuestion } from '../../../models/interfaces'
   templateUrl:'./answer-type-list.component.html'
 })
 export class AnswerListViewComponent {
-  @Input()
-  question: BaseQuestion
+  @Input() listOfQuestions:Array<String>;
 
   listOfQuestion = ["The box will stick to the conveyor belt, and reach speed v = 6.2 m/s instantaneosly.","The box will sit motionless for a few seconds until enough force builds up to change its velocity.","The box will begin to accelerate as soon as it touches the conveyor. The belt will slide underneath the box, accelerating the box uniformly. Then, the box will reach the same speed as the belt, and the box will stop accelerating","The box will accelerate less as it approaches the speed of the conveyor. So, the box will come closer and closer to the speed of the belt, but it will never quite"]
 
