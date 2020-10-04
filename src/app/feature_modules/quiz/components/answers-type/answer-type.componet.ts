0cde25749e1af76144412b1fca3d7270a68ae9e2
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-10-05 00:39:33 +0700

    answear compoent stand as wrapper 😂

---
+++ src/app/feature_modules/quiz/components/answers-type/answer-type.componet.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {AnswerListViewComponent} from './list/answer-type-list.component'
+import { AnswerTextViewComponent } from "./input-box/answer-type-input-box.component";
+@Component({
+  selector: 'app-quiz-answear-type',
+  template:`
+  <div #textView *ngIf="this.answearType == 'TEXT';else listView">
+    <app-quiz-answer-type-input-text></app-quiz-answer-type-input-text>
+    <div class="flex flex-row justify-between px-3 m-4" style="min-width: 300px;">
+            <button 
+            style="min-width: 170px;"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Kiểm tra kết quả
+            </button>
+            <button *ngIf="this.hasHelp" (click)="toggleHelp()" class="bg-gray-200 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Trợ giúp</button>  
+    </div>
+  </div>
+  <ng-template #listView>
+    <app-quiz-answer-type-list [listOfQuestions]="this.answearContent"></app-quiz-answer-type-list>
+    <div class="flex flex-row justify-between px-3 m-4" style="min-width: 300px;">
+            <button 
+            style="min-width: 170px;"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Kiểm tra kết quả
+            </button>
+            <button *ngIf="this.hasHelp == null" class="bg-gray-200 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">Giợi ý</button>  
+    </div>
+    </ng-template>
+  
+  `
+})
+export class AnswearTypeComponent {
+    @Input() answearType:string;
+    @Input() hasHelp:boolean;
+
+    @Input() answearContent?:string;
+    @Output() enableHelp = new EventEmitter
+
+    toggleHelp(){
+        this.enableHelp.emit()
+    }
+}
