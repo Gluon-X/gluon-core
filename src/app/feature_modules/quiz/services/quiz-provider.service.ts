b5ccabdb155da60a5a5f885ce3c11f86e0f4567f
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-10-05 21:42:59 +0700

    quick fix to generate DummyData

--- src/app/feature_modules/quiz/services/quiz-provider.service.ts
+++ src/app/feature_modules/quiz/services/quiz-provider.service.ts
@@ -17,6 +17,7 @@ export class QuizStorage {
     return new Promise<MainQuestion>((resolve, reject) => {
       setTimeout(() => {
         const data = realDummyData[qid]
+        console.log(data)
         if (isNullOrUndefined(data)) reject()
         else resolve(data)
       }, 1000)
@@ -36,11 +37,12 @@ export class QuizHandler implements QuizPlayable {
       this._qid = null
       return
     }
+    this.parse(realDummyData[value])
     // fetching data from https server
-    this._quizStorage
-      .get(value)
-      .then(this.parse)
-      .catch(() => (this._isError = true))
+    // this._quizStorage
+    //   .get(value)
+    //   .then(this.parse)
+    //   .catch(() => (this._isError = true))
   }
 
   get qid(): string {
