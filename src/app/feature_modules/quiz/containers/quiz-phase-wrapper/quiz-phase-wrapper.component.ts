ceceb48c435f1abf32c8778e444113675cbc9677
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-10-05 21:45:12 +0700

    quiz-phase inject services

--- src/app/feature_modules/quiz/containers/quiz-phase-wrapper/quiz-phase-wrapper.component.ts
+++ src/app/feature_modules/quiz/containers/quiz-phase-wrapper/quiz-phase-wrapper.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { PhaseStack } from '../../models/interfaces';
+import { ThrowStmt } from '@angular/compiler'
+import { Component, Input, OnInit } from '@angular/core'
+import {
+  MultiPhasesProvider,
+  MultiQuestionsProvider,
+} from '../../models/classes'
+import { PhaseStack } from '../../models/interfaces'
 @Component({
   // Render [subQuestions] xN
   selector: 'app-quiz-phase-wrapper',
   templateUrl: './quiz-phase-wrapper.component.html',
-  styleUrls: ['./quiz-phase-wrapper.component.scss']
+  styleUrls: ['./quiz-phase-wrapper.component.scss'],
 })
-
-
 export class QuizPhaseWrapperComponent implements OnInit {
-  @Input() phaseData:PhaseStack;
-  constructor() { }
+  @Input() phaseProvider: MultiPhasesProvider
+  @Input() multiQuestionProvider: MultiQuestionsProvider
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  getPhaseList(){
-    return Object.keys(this.phaseData);
+  ngOnInit(): void {}
+  getPhaseList() {
+    return Array(this.phaseProvider.phasesCount).fill(0)
   }
 
+  test() {
+    this.phaseProvider.submit(2)
+    this.phaseProvider.isCorrect
+    this.phaseProvider.next()
+    this.phaseProvider.submit(1)
+  }
 }
