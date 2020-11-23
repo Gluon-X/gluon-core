ef0d7c11b2f7eb50fc4e97d616bc75f34cdadfc4
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-11-24 04:21:40 +0700

    child call up for there T.submit() 😌

--- src/app/feature_modules/quiz/containers/quiz-main-question/quiz-main-question.component.ts
+++ src/app/feature_modules/quiz/containers/quiz-main-question/quiz-main-question.component.ts
@@ -1,40 +1,58 @@
+import { ThrowStmt } from '@angular/compiler'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
-import { QuestionProvider } from '../../models'
+import { retry } from 'rxjs/operators'
+import { Box, MultiBoxesProvider, MultiPhasesProvider, MultipleChoicesProvider, PossibleInputAnswer, QuestionProvider, ShortAnswerProvider, Submitable } from '../../models'
 
 @Component({
   selector: 'app-quiz-main-question',
   templateUrl: './quiz-main-question.component.html',
   styleUrls: ['./quiz-main-question.component.scss'],
 })
-export class QuizMainQuestionComponent implements OnInit {
+export class QuizMainQuestionComponent {
+  @Input()
+  question: Box;
+
   @Input()
   isMainQuestion = true
 
   @Output()
   return = new EventEmitter()
 
-  @Input() mainQuestionProvider: QuestionProvider
-
   @Input() quizTitle: string
 
-  // tslint:disable-next-line:no-output-on-prefix
-  @Output() onMainQuestionSubmition = new EventEmitter()
-
-  // tslint:disable-next-line:no-output-on-prefix
-  @Output() onHelp = new EventEmitter()
-
-  isEnableHelp: boolean = false
-
-  ngOnInit(): void {
-    // this.enableHelp();
+  get instance() {
+    console.log(this.question)
+    if (this.question instanceof MultiBoxesProvider || this.question instanceof MultiPhasesProvider) {
+      return (this.question as any).current
+    }
+    return this.question
   }
 
-  submitAnswer(event) {
-    this.onMainQuestionSubmition.emit(event)
+  submission(answear: PossibleInputAnswer) {
+    console.log(answear);
+    if (this.question instanceof MultiPhasesProvider) {
+      ((this.question as any) as MultiPhasesProvider).submit(answear);
+      // ((this.question as any) as MultiPhasesProvider).next();
+      // console.log(((this.question as any) as MultiPhasesProvider).isCorrect)
+    } else {
+      if ((this.question instanceof ShortAnswerProvider)) {
+        console.log("OK");
+        (this.question as ShortAnswerProvider).submit(answear);
+
+      }
+      if ((this.question instanceof MultipleChoicesProvider)) {
+        (this.question as MultipleChoicesProvider).submit(answear as number);
+
+      }
+
+    }
   }
 
-  enableHelp() {
-    this.isEnableHelp = !this.isEnableHelp
-    this.onHelp.emit()
+  praseMulthiPhaseProvider(): MultiPhasesProvider | undefined {
+    if (this.question instanceof MultiPhasesProvider) {
+      return this.question as MultiPhasesProvider;
+    }
+    return undefined
   }
+
 }
