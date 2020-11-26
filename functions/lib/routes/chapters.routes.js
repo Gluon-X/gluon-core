7cfa43e8eafa96fe12170894eb507677969c2c55
Zeroska <owlthres@gmail.com>
2020-11-26 14:10:00 +0700

    Refactor some unuse api route

--- functions/lib/routes/chapters.routes.js
+++ functions/lib/routes/chapters.routes.js
@@ -12,13 +12,18 @@ class ChapterRoute {
     }
 
     chapterRoutes(){
-      const chapterCollection = "Chapter"
-        //Get all the chapter in one hit
-      this.routes.get('/chapters',async (req,res)=>{
-        try {
-          console.log("[*] GET chapters")
-          const snapshot = await admin.firestore().collection(chapterCollection).get()
+      const chapterCollection = "Chapters"
+      const exerciseCollection  = "Exercises"
+      console.log("[*] Chapter Route works!")
 
+      // GET specific chapter
+      this.routes.get('/chapters/:chapterId',async (req,res)=>{
+        // Return a collection of exercise
+        // Note should use the exercise API, because chapter is fixed and not even a collection in the database
+        try {
+          console.log("[*] GET chapters exercise")
+          const {chapterId} = req.params
+          const snapshot = await admin.firestore().collection(exerciseCollection).where("chapterId","==",chapterId).get()
           if(snapshot.empty){
             throw new Error('Collection Chapter not found')
           }
@@ -39,18 +44,19 @@ class ChapterRoute {
           })
         }
       })
-      //This is adding the chapter
+
+      // WILL DELETE SOON -> because chapter is fixed
       this.routes.post('/chapters',async (req,res)=>{
         try {
             console.log('[*] POST chapter')
             if (!('name ' in req.body)){
               throw new Error('Name not found')
             }
+
             // Not adding any question because you need to create a chapter before you can add any question
             const newChapter = {
               chapterId: req.body['chapterId'],
               name: req.body['name'],
-              //question is contain all the
               questions: req.body['questions'],
               description: req.body['description'],
               relatedResources: req.body['relatedResources'],
